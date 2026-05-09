dd985ff0e5072f091f3d6c206f31f9c6f489f5d2
chloorophyll <franceschloe.talaman-23@cpu.edu.ph>
2026-05-09 10:37:42 +0800

    Edited classes

--- src/classes/Transferee.ts
+++ src/classes/Transferee.ts
@@ -1,32 +1,3 @@
 import { Student } from './Student'
 
 export class Transferee extends Student {
-  private _tuitionFee: number
-  private _previousSchool: string
-
-  constructor(id: string, name: string, yearLevel: number, previousSchool: string) {
-    super(id, name, yearLevel)
-    this._tuitionFee = 25000
-    this._previousSchool = previousSchool
-  }
-
-  getPreviousSchool(): string {
-    return this._previousSchool
-  }
-
-  computeTuition(): number {
-    return this._tuitionFee + 5000
-  }
-
-  getStatus(): string {
-    return `Transferee - From ${this._previousSchool}`
-  }
-
-  getInfo(): string {
-    return `${this.getName()} | ID: ${this.getId()} | Year: ${this.getYearLevel()} | ${this.getStatus()} | Tuition: PHP ${this.computeTuition()}`
-  }
-
-  getRole(): string {
-    return 'Student'
-  }
-}
